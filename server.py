14eecbb285c05a92dbf4d8b44a58b1235b388903
Bruno Badaró <maturegt@gmail.com>
2023-09-11 02:43:13 -0300

    Auto unload
    will unload the LLM from GPU memory if the server doesnt get any request for 3 minute

--- server.py
+++ server.py
@@ -3,9 +3,24 @@ import json
 # Import the Flask library
 from flask import Flask, request, jsonify
 from flask_cors import cross_origin, CORS
-
-#import time to get tokens per second
+from flask_apscheduler import APScheduler
 import time
+
+llm = None
+last_request_time = time.time()
+#Location of the model
+ourmodel = "../../AI/models/converted.bin"
+#model type
+model_type="llama"
+#how many gpu_layers to use
+gpu_layers=25 
+#how many tokens to generate
+max_new_tokens=256
+#temperature of the model
+temperature=0.8
+#repetition penalty
+repetition_penalty=1.1
+scheduler = APScheduler()
 
 # Create a Flask app object
 app = Flask(__name__)
@@ -24,11 +39,19 @@ def generate():
     return jsonify(output)
 
 
-
 def gettingAlpaca(prompt):
     #start the timer
     start_time = time.time()
-    #send the prompt to the model
+    
+    global last_request_time
+    # Update the last request time with the current time
+    last_request_time = time.time()
+    # If the model is not loaded, load it first
+    global llm
+    if llm is None:
+        print("Model is not loaded!")
+        llm = load_model()
+    #send the prompt to the model and get the output
     output = llm(prompt)
     #get the number of tokens
     num_tokens = len(output)
@@ -43,32 +66,47 @@ def gettingAlpaca(prompt):
     #print the tokens per second
     print(f"\n\n#Tokens per second: {tokens_per_second}")
     return output
-    
 
 
-#Location of the model
-ourmodel = "../../AI/models/converted.bin"
-#model type
-model_type="llama"
-#how many gpu_layers to use
-gpu_layers=25 
-#how many tokens to generate
-max_new_tokens=256
-#temperature of the model
-temperature=0.8
-#repetition penalty
-repetition_penalty=1.1
+def load_model():
+    #load the model
+    print("Loading model...")
+    # Use ctransformers to load the model
+    global llm
+    llm = AutoModelForCausalLM.from_pretrained(ourmodel, model_type=model_type, gpu_layers=gpu_layers, max_new_tokens=max_new_tokens,temperature=temperature,repetition_penalty=repetition_penalty) 
+    print("Model loaded!")
+    return llm
 
-#load the model
-print("Loading model...")
-# Use ctransformers to load the model
-llm = AutoModelForCausalLM.from_pretrained(ourmodel, model_type=model_type, gpu_layers=gpu_layers, max_new_tokens=max_new_tokens,temperature=temperature,repetition_penalty=repetition_penalty) 
-print("Model loaded!")
+def unload_model():
+    # Unload the model
+    global llm
+    llm = None
+    # Print a message to confirm
+    print("Model unloaded!")
+
+def check_and_unload():
+    global last_request_time
+    global llm
+    #get the current time
+    current_time = time.time()
+    #calculate the difference in seconds
+    difference = current_time - last_request_time
+    #if the difference is more than 60 seconds, unload the model
+    if llm is not None and difference > 180:
+        llm = unload_model()
 
 
-
+scheduler.add_job(func=check_and_unload, trigger='interval', seconds=60, id='check_and_unload_job')
 
 
 # Run the app on port 5000 with debug mode on
 if __name__ == "__main__":
+    scheduler.init_app(app)
+    scheduler.start()
     app.run(port=5000, debug=True)
+    
+    
+
+
+
+
